a8d4d478fce0dd0c9d39227c2a332c6de33d4c3d
Sunil Dagur <sunildagur566@gmail.com>
2023-09-06 22:07:54 +0530

    Add project files.

---
+++ Assignment3(1)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class MyClass
+{
+    private int privateField;
+
+    public void SetValue(int value)
+    {
+        privateField = value;
+    }
+
+    public int GetValue()
+    {
+        return privateField;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass();
+
+        myObject.SetValue(58);
+
+        int value = myObject.GetValue();
+        Console.WriteLine("The value is: " + value);
+    }
+}
